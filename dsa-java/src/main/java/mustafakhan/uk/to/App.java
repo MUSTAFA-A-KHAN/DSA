03ec8827e2941f04b1da9e74314af9132321c1bc
MUSTAFA-A-KHAN <70553059+MUSTAFA-A-KHAN@users.noreply.github.com>
2024-09-04 10:45:09 +0000

    feat: add the arrayutilities to perform operations in a better(handful) way.

--- dsa-java/src/main/java/mustafakhan/uk/to/App.java
+++ dsa-java/src/main/java/mustafakhan/uk/to/App.java
@@ -2,26 +2,12 @@ package mustafakhan.uk.to;
 
 import java.util.Scanner;
 
-import mustafakhan.uk.to.dsa.dataStructures.arrays.*;
+import mustafakhan.uk.to.dsa.dataStructures.arrays.ArraysUtilities;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int length = scanner.nextInt();
-        ArraysOperations arraysOperations = new ArraysOperations(length);
-        for (int i = 0; i < length; i++) {
-            arraysOperations.addElement(scanner.nextInt());
-        }
-
-        System.out.println("Element at index: ");
-        int index = scanner.nextInt();
-        arraysOperations.getElement(index);
-        scanner.close();
-
         
-    }
+       new ArraysUtilities().ArraysUtils();
+        
+}
 }
---
+++ dsa-java/src/main/java/mustafakhan/uk/to/dsa/dataStructures/arrays/ArraysUtilities.java
@@ -0,0 +1,68 @@
+package mustafakhan.uk.to.dsa.dataStructures.arrays;
+
+import java.util.Scanner;
+
+public class ArraysUtilities {
+    public Integer DoOPerations(int choice, int index, int element, ArraysOperations arraysOperations) {
+        switch (choice) {
+            case 1:
+                return arraysOperations.getElement(index);
+            // break;
+            case 2:
+                arraysOperations.updateElement(index, element);
+                break;
+            case 3:
+                arraysOperations.deleteElement(index);
+                break;
+            case 4:
+                arraysOperations.addElement(element);
+                break;
+            case 5:
+                return arraysOperations.getSize();
+            // break;
+        }
+        return null;
+    }
+
+    public void ArraysUtils() {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        ArraysOperations arraysOperations = new ArraysOperations(length);
+
+        int choice = -1, index = -1, element = -1;
+        do {
+            System.out.println("\nArray Operations Menu:");
+            System.out.println("1. Get Element");
+            System.out.println("2. Update Element");
+            System.out.println("3. Delete Element");
+            System.out.println("4. Add Element");
+            System.out.println("5. Get Size");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            if (choice == 0) {
+                break;
+            }
+            System.out.print("Enter index (or -1 if not applicable): ");
+            index = scanner.nextInt();
+            element = -1;
+            if (choice == 2 || choice == 4) {
+                System.out.println("Enter element:");
+                element = scanner.nextInt();
+            }
+           
+           Integer result = DoOPerations(choice, index, element, arraysOperations);
+            if (result != null) {
+                if (choice == 1) {
+                    System.out.println("Element at index " + index + ": " + result);
+                } else if (choice == 5) {
+                    System.out.println("Array size: " + result);
+                }
+            }
+        } while (choice != 0);
+
+        scanner.close();
+    }
+
+}
