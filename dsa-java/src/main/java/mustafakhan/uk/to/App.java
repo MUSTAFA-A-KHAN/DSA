5b0245e5623697d5bac66aa700f2c3bb948a5964
MUSTAFA-A-KHAN <70553059+MUSTAFA-A-KHAN@users.noreply.github.com>
2024-09-04 06:31:19 +0000

    feat: simple array operations

--- dsa-java/src/main/java/mustafakhan/uk/to/App.java
+++ dsa-java/src/main/java/mustafakhan/uk/to/App.java
@@ -1,13 +1,27 @@
 package mustafakhan.uk.to;
 
+import java.util.Scanner;
+
+import mustafakhan.uk.to.dsa.dataStructures.arrays.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        ArraysOperations arraysOperations = new ArraysOperations(length);
+        for (int i = 0; i < length; i++) {
+            arraysOperations.addElement(scanner.nextInt());
+        }
+
+        System.out.println("Element at index: ");
+        int index = scanner.nextInt();
+        arraysOperations.getElement(index);
+        scanner.close();
+
+        
     }
 }
