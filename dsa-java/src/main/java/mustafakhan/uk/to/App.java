ac0bf4a54202bd95f5fa89920d6895c37a40654d
MUSTAFA-A-KHAN <70553059+MUSTAFA-A-KHAN@users.noreply.github.com>
2024-09-10 10:53:18 +0000

    feat: Switch Case for DS input

--- dsa-java/src/main/java/mustafakhan/uk/to/App.java
+++ dsa-java/src/main/java/mustafakhan/uk/to/App.java
@@ -2,12 +2,28 @@ package mustafakhan.uk.to;
 
 import java.util.Scanner;
 
+import mustafakhan.uk.to.dsa.dataStructures.ArrayList.ArrayListUtilities;
 import mustafakhan.uk.to.dsa.dataStructures.arrays.ArraysUtilities;
 
 public class App {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int choice=sc.nextInt();
+        switch (choice) {
+            case 1:
+                new ArraysUtilities().ArraysUtils();
+                break;
+            
+            case 2:
+                new ArrayListUtilities().ArrayListUtils();
+                break;
         
-       new ArraysUtilities().ArraysUtils();
+            default:
+                break;
+        }
+        
+       
+       
         
 }
 }
